cb2e53c12aea68164e68437f0b27164f2e5033eb
aldoprogrammer <aldobesma@gmail.com>
2024-06-09 16:06:00 +0700

    chore: Update backend proxy target port in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:4000', // Adjust if your backend runs on a different port
+        target: 'http://localhost:3000', // Adjust if your backend runs on a different port
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
